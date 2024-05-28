448ef3330b0bca6d4755f45ae5e469a631090558
Ali Mohamed <amohamd31@gmail.com>
2024-05-29 01:57:51 +0300

    Merge master into main

--- src/main/java/com/codewithayii/contactapi/Main.java
+++ src/main/java/com/codewithayii/contactapi/Main.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.codewithayii.repo")
 public class Main {
-
+// Yooo
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
